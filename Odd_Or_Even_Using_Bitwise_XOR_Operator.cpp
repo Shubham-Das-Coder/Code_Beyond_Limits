5542d7907c755007660d0486ef9557c1eb331ba4
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-11-18 21:28:22 +0530

    Add files via upload

---
+++ Odd_Or_Even_Using_Bitwise_XOR_Operator.cpp
@@ -0,0 +1,28 @@
+// C++ program to check for even or odd
+// using Bitwise XOR operator
+
+#include <iostream>
+using namespace std;
+
+// Returns true if n is even, else odd
+bool isEven(int n)
+{
+
+    // n^1 is n+1, then even, else odd
+    if (n ^ 1 == n + 1)
+        return true;
+    else
+        return false;
+}
+
+// Driver code
+int main()
+{
+    int n;
+    cin >> n;
+    isEven(n)
+        ? cout << "Even"
+        : cout << "Odd";
+
+    return 0;
+}
