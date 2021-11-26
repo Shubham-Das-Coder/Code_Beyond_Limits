cd30672523280ce1d397135bf70025dc883ae027
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-11-26 18:12:19 +0530

    Add files via upload

---
+++ String_4.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <iostream>
+using namespace std;
+int main()
+{
+    string str;
+    cout << "Enter your name : ";
+    cin >> str;
+    cout << str << endl;
+    return 0;
+}
