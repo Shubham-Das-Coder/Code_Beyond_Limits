22f762942ef22f04bad76c07e87b69050bde49f4
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-11-26 19:42:25 +0530

    Add files via upload

---
+++ String_12.cpp
@@ -0,0 +1,17 @@
+/*
+Time complexity of compare() function is big O(n+m)
+where n is the size of the first string and m is the size of the second string
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s1 = "abc";
+    string s2 = "xyz";
+    string s3 = "abc";
+    cout << s1.compare(s2) << endl;
+    cout << s1.compare(s3) << endl;
+    cout << s2.compare(s1) << endl;
+    return 0;
+}
