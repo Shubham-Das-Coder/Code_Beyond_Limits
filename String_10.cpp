741004e0fc522f85c2a95b34a6e88570fb580cc5
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-11-26 19:31:25 +0530

    Add files via upload

---
+++ String_10.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string name = "Shubham";
+    cout << name.at(3) << endl;
+    cout << name[3] << endl;
+    return 0;
+}
