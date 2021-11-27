fd0187646ba3b3c05bd17cdfe70a029f7a55d1ec
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-11-27 13:56:05 +0530

    Add files via upload

---
+++ Linear_2_Edited.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int key, n;
+    cout << "Enter the legth of the array : " << endl;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Enter the element you want to search : " << endl;
+    cin >> key;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == key)
+        {
+            cout << "Found at : " << i + 1 << endl;
+        }
+    }
+    return 0;
+}
